63703f0819d7ea974459e3c1d75ff0e8f412de2d
Zain Ul Arifeen <zayn.ul.arifeen2412@gmail.com>
2026-08-05 07:28:42 +0500

    Add operators lesson

---
+++ 01-Basics/operators.py
@@ -0,0 +1,13 @@
+#Basic Operators in python
+a=eval(input("enter a number:"))
+b=eval(input(" enter the second number "))
+print("Sum =", a+b)
+print("Difference =", a-b)
+print("Product =", a*b)
+print("Quotient =", a/b)
+print("Floor Division =", a//b)
+print("Modulus =", a%b)
+print("Exponent =", a**b)
+print("Type of a is", type(a))
+print("Type of b is", type(b))
+
